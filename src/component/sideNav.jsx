2cd85d3dfe16928c6ac8e61735caf1a3e8251ac9
crihit <rlagusgkr98@gmail.com>
2021-08-04 17:51:28 +0900

    router and view update

--- src/component/sideNav.jsx
+++ src/component/sideNav.jsx
@@ -49,6 +49,8 @@ const SideNav = (props) => {
                 history.push('/admin/attraction')
             } else if (key === 'event') {
                 history.push('/admin/event')
+            } else if (key === 'main') {
+                history.push('/restaurant')
             }
         } else {
             if (key === 'restaurant') {
@@ -59,6 +61,8 @@ const SideNav = (props) => {
                 history.push('/event')
             } else if (key === 'admin') {
                 history.push('/admin')
+            } else if (key === 'main') {
+                history.push('/restaurant')
             }
         }
 
@@ -89,7 +93,7 @@ const SideNav = (props) => {
                         marginTop: '15px'
                     }}
                 >
-                    <a href="/#/restaurant">TRIPIS</a>
+                    <a onClick={(e) => clickHandler('main')}>TRIPIS</a>
                 </div>
             </SidebarHeader>
             <SidebarContent className={classes.content}>
