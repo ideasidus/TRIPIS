40587fe6460723912784cbe5727e2170d48e396b
crihit <rlagusgkr98@gmail.com>
2021-08-05 14:42:45 +0900

    bug fix

--- src/component/test.jsx
+++ src/component/test.jsx
@@ -21,65 +21,67 @@ const Test = (props) => {
     }
 
     return (
-        <ProSidebar
-            toggle={true}
-            collapsed={true}
-            style={{
-                height: '100vh',
-            }}
-        >
-            <SidebarHeader>
-                <div
-                    style={{
-                        padding: '24px',
-                        textTransform: 'uppercase',
-                        fontWeight: 'bold',
-                        fontSize: 14,
-                        letterSpacing: '1px',
-                        // overflow: 'auto',
-                        // textOverflow: 'ellipsis',
-                        whiteSpace: 'nowrap',
-                    }}
-                >
-                    TRIPS
-                </div>
-            </SidebarHeader>
-            <SidebarContent>
-                <Menu iconShape="square">
-                    <SubMenu title="Weather" icon={<MailIcon />}>
-                        <MenuItem>Weather 정보 ~~ </MenuItem>
-                    </SubMenu>
-                    <SubMenu title="Restaurant" icon={<MailIcon />}>
-                        <MenuItem>Restaurant</MenuItem>
-                    </SubMenu>
-                    <SubMenu title="Attraction" icon={<MailIcon />}>
-                        <MenuItem>Attraction</MenuItem>
-                    </SubMenu>
-                    <SubMenu title="Event" icon={<MailIcon />}>
-                        <MenuItem>Event</MenuItem>
-                    </SubMenu>
-                </Menu>
-            </SidebarContent>
+        // <ProSidebar
+        //     toggle={true}
+        //     collapsed={true}
+        //     style={{
+        //         height: '100vh',
+        //     }}
+        // >
+        //     <SidebarHeader>
+        //         <div
+        //             style={{
+        //                 padding: '24px',
+        //                 textTransform: 'uppercase',
+        //                 fontWeight: 'bold',
+        //                 fontSize: 14,
+        //                 letterSpacing: '1px',
+        //                 // overflow: 'auto',
+        //                 // textOverflow: 'ellipsis',
+        //                 whiteSpace: 'nowrap',
+        //             }}
+        //         >
+        //             TRIPS
+        //         </div>
+        //     </SidebarHeader>
+        //     <SidebarContent>
+        //         <Menu iconShape="square">
+        //             <SubMenu title="Weather" icon={<MailIcon />}>
+        //                 <MenuItem>Weather 정보 ~~ </MenuItem>
+        //             </SubMenu>
+        //             <SubMenu title="Restaurant" icon={<MailIcon />}>
+        //                 <MenuItem>Restaurant</MenuItem>
+        //             </SubMenu>
+        //             <SubMenu title="Attraction" icon={<MailIcon />}>
+        //                 <MenuItem>Attraction</MenuItem>
+        //             </SubMenu>
+        //             <SubMenu title="Event" icon={<MailIcon />}>
+        //                 <MenuItem>Event</MenuItem>
+        //             </SubMenu>
+        //         </Menu>
+        //     </SidebarContent>
 
-            <SidebarFooter style={{ textAlign: 'center' }}>
-                <div
-                    className="sidebar-btn-wrapper"
-                    style={{
-                        padding: '20px 24px',
-                    }}
-                >
-                    <a
-                        href="https://github.com/ideasidus/TRIPIS"
-                        target="_blank"
-                        className="sidebar-btn"
-                        rel="noopener noreferrer"
-                    >
-                        <GitHubIcon />
-                        {/* <span> {intl.formatMessage({ id: 'viewSource' })}</span> */}
-                    </a>
-                </div>
-            </SidebarFooter>
-        </ProSidebar>
+        //     <SidebarFooter style={{ textAlign: 'center' }}>
+        //         <div
+        //             className="sidebar-btn-wrapper"
+        //             style={{
+        //                 padding: '20px 24px',
+        //             }}
+        //         >
+        //             <a
+        //                 href="https://github.com/ideasidus/TRIPIS"
+        //                 target="_blank"
+        //                 className="sidebar-btn"
+        //                 rel="noopener noreferrer"
+        //             >
+        //                 <GitHubIcon />
+        //                 {/* <span> {intl.formatMessage({ id: 'viewSource' })}</span> */}
+        //             </a>
+        //         </div>
+        //     </SidebarFooter>
+        // </ProSidebar>
+
+
         // <Drawer
         //     variant="permanent"
         // >
@@ -102,6 +104,7 @@ const Test = (props) => {
         //     </List>
 
         // </Drawer>
+        <h4></h4>
     )
 }
 
